ba1da060b3f4eb9920d7d3f58fcd947efddd171a
Ruchikavenkat <ruchika.ai21@bmsce.ac.in>
2023-08-30 20:41:52 +0530

    gaussian blur

---
+++ noise.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+
+# Define the input directory where your noisy images are located
+input_directory = './output_train'
+
+# Define the output directory where the blurred images will be saved
+output_directory = './noise_reduction'
+
+# Ensure the output directory exists
+os.makedirs(output_directory, exist_ok=True)
+
+# Function to apply Gaussian blur to an image
+def apply_gaussian_blur(image_path, output_path, kernel_size=(5, 5), sigmaX=0):
+    # Read the image
+    image = cv2.imread(image_path)
+    
+    # Apply Gaussian blur
+    blurred_image = cv2.GaussianBlur(image, kernel_size, sigmaX)
+    
+    # Save the blurred image to the output directory
+    cv2.imwrite(output_path, blurred_image)
+
+# Loop through the images in the input directory
+for filename in os.listdir(input_directory):
+    if filename.endswith('.jpg'):
+        # Create full paths for input and output images
+        input_image_path = os.path.join(input_directory, filename)
+        output_image_path = os.path.join(output_directory, filename)
+        
+        # Apply Gaussian blur to the image
+        apply_gaussian_blur(input_image_path, output_image_path)
